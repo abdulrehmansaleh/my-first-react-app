0d2096465227640018ea4af85999ff81d32bf419
commando <salehabdulrehman1@gmail.com>
2025-12-16 18:36:42 -0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-react-app/"
+  base: "/my-first-react-app/"
 })
